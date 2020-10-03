3054efd72626db0376aed23ddcb403b47d94e904
Britt Henderson <Bleggaman@yahoo.com>
2020-10-02 20:31:55 -0700

    added boundaries and kind of a camera movement system?

---
+++ Assets/Scripts/Boundary.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boundary : MonoBehaviour {
+
+    public GameObject cam;
+    public GameObject area;
+
+
+    void OnTriggerEnter2D(Collider2D col) {
+        Debug.Log("Bleg");
+        this.cam.transform.position = area.transform.position;
+        this.cam.transform.rotation = Quaternion.Euler(0, 0, -45) * cam.transform.rotation;
+    }
+}
