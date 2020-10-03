880594e357c044478710061cc9d3e45e259f3cfa
RetroSpecter <InsertNameHere4897@gmail.com>
2020-10-02 17:32:51 -0700

    Added spherical physics

---
+++ Assets/Player/Controller2D.cs
@@ -0,0 +1,233 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(BoxCollider2D))]
+public class Controller2D : MonoBehaviour {
+
+
+    public LayerMask collisionMask;
+    public LayerMask triggerMask;
+
+    const float skinWidth = 0.015f;
+
+    BoxCollider2D collider;
+    RaycastOrigins raycastOrigin;
+    public int horizontalRayCount = 4;
+    public int VerticalRayCount = 4;
+
+    float horizontalRaySpacing;
+    float VecticalRaySpacing;
+    public CollisionInfo collisions;
+    public Transform centerOfGravity;
+    public float extraTriggerRadius = 0.1f;
+    public bool collisionEnabled = true;
+
+    public Vector2 dirOfGravity;
+
+    void Awake()
+    {
+        collider = GetComponent<BoxCollider2D>();
+        CalculateRaySpacing();
+    }
+
+    public void Move(Vector3 velocity)
+    {
+        UpdateRaycastOrigins();
+        collisions.Reset();
+
+        if (collisionEnabled)
+        {
+            if (centerOfGravity != null)
+            {
+                dirOfGravity = centerOfGravity.transform.position - transform.position;
+            }
+            else
+            {
+                dirOfGravity = -transform.position;
+            }
+
+            if (velocity.x != 0)
+            {
+                HorizontalTriggers(velocity);
+                HorizontalCollisions(ref velocity);
+            }
+
+            if (velocity.y != 0)
+            {
+                VerticalTriggers(velocity);
+                VerticalCollisions(ref velocity);
+            }
+        }
+        CalculateGravity();
+        transform.Translate(velocity);
+    }
+
+    // for this I want constantly checking there surroundings for stuff;
+    public void triggerCheck() {
+        Debug.DrawRay(transform.position, Vector3.up * extraTriggerRadius, Color.green);
+        Collider2D hit = Physics2D.OverlapCircle(transform.position, extraTriggerRadius, triggerMask);
+        if (hit != null) {
+            hit.transform.gameObject.SendMessageUpwards("OnTrigger", this.transform, SendMessageOptions.DontRequireReceiver);
+            SendMessageUpwards("OnTrigger", hit.transform, SendMessageOptions.DontRequireReceiver);
+        }
+    }
+
+    void CalculateGravity()
+    {
+        //RaycastHit2D hit = Physics2D.Raycast(raycastOrigin.bottomLeft, -transform.up, 1, collisionMask);
+
+        //Debug.DrawRay(transform.position, hit.normal, Color.green);
+        Debug.DrawRay(transform.position, dirOfGravity, Color.blue);
+
+        //if (hit)
+        //dirOfGravity = -hit.normal;
+
+        transform.rotation = Quaternion.FromToRotation(Vector3.up, -Vector3.Normalize(dirOfGravity));
+
+    }
+
+    //TODO: note there is aweakness in how i am detecting collisions and triggers. multiple raycasts hitting an object results in it being caleed multiple times
+    void HorizontalCollisions(ref Vector3 velocity)
+    {
+        float directionX = Mathf.Sign(velocity.x);
+        float rayLength = Mathf.Abs(velocity.x) + skinWidth;
+
+        for (int i = 0; i < VerticalRayCount; i++)
+        {
+            Vector2 rayOrigin = (directionX == -1) ? raycastOrigin.bottomLeft : raycastOrigin.topRight;
+            rayOrigin += (Vector2)transform.up * -directionX * (horizontalRaySpacing * i);
+            RaycastHit2D hit = Physics2D.Raycast(rayOrigin, transform.right * directionX, rayLength, collisionMask);
+
+            Debug.DrawRay(rayOrigin, directionX * transform.right * rayLength, Color.red);
+
+            if (hit)
+            {
+                velocity.x = (hit.distance - skinWidth) * directionX;
+                rayLength = hit.distance;
+
+                collisions.left = directionX == -1;
+                collisions.right = directionX == 1;
+
+
+                hit.transform.gameObject.SendMessageUpwards("OnCollision", this.transform, SendMessageOptions.DontRequireReceiver);
+                SendMessageUpwards("OnCollision", hit.transform, SendMessageOptions.DontRequireReceiver);
+            }
+        }
+    }
+
+    void VerticalCollisions(ref Vector3 velocity)
+    {
+        float directionY = Mathf.Sign(velocity.y);
+        float rayLength = Mathf.Abs(velocity.y) + skinWidth;
+
+        for (int i = 0; i < VerticalRayCount; i++)
+        {
+            Vector2 rayOrigin = (directionY == -1) ? raycastOrigin.bottomLeft : raycastOrigin.topRight;
+            rayOrigin += (Vector2)(-directionY * transform.right) * (VecticalRaySpacing * i + velocity.x);
+            RaycastHit2D hit = Physics2D.Raycast(rayOrigin, transform.up * directionY, rayLength, collisionMask);
+
+            Debug.DrawRay(rayOrigin, transform.up * 10 * rayLength, Color.red);
+
+            if (hit)
+            {
+                velocity.y = (hit.distance - skinWidth) * directionY;
+                rayLength = hit.distance;
+
+                collisions.below = directionY == -1;
+                collisions.above = directionY == 1;
+
+                hit.transform.gameObject.SendMessage("OnCollision", this.transform, SendMessageOptions.DontRequireReceiver);
+                SendMessage("OnCollision", hit.transform, SendMessageOptions.DontRequireReceiver);
+            }
+        }
+    }
+
+    void HorizontalTriggers(Vector3 velocity)
+    {
+        float directionX = Mathf.Sign(velocity.x);
+        float rayLength = Mathf.Abs(velocity.x) + skinWidth;
+
+        for (int i = 0; i < VerticalRayCount; i++)
+        {
+            Vector2 rayOrigin = (directionX == -1) ? raycastOrigin.bottomLeft : raycastOrigin.BottomRight;
+            rayOrigin += (Vector2)transform.up * (horizontalRaySpacing * i);
+            RaycastHit2D hit = Physics2D.Raycast(rayOrigin, transform.right * directionX, rayLength, triggerMask);
+
+            //Debug.DrawRay(rayOrigin, transform.right * directionX * rayLength, Color.red);
+
+            if (hit)
+            {
+                hit.transform.gameObject.SendMessageUpwards("OnTrigger", this.transform, SendMessageOptions.DontRequireReceiver);
+                SendMessageUpwards("OnTrigger", hit.transform, SendMessageOptions.DontRequireReceiver);
+                break;
+            }
+        }
+    }
+
+    void VerticalTriggers(Vector3 velocity)
+    {
+        float directionY = Mathf.Sign(velocity.y);
+        float rayLength = Mathf.Abs(velocity.y) + skinWidth;
+
+        for (int i = 0; i < VerticalRayCount; i++)
+        {
+            Vector2 rayOrigin = (directionY == -1) ? raycastOrigin.bottomLeft : raycastOrigin.topLeft;
+            rayOrigin += (Vector2)transform.right * (VecticalRaySpacing * i + velocity.x);
+            RaycastHit2D hit = Physics2D.Raycast(rayOrigin, transform.up * directionY, rayLength, triggerMask);
+
+            //Debug.DrawRay(rayOrigin, Vector2.up * directionY * rayLength, Color.red);
+
+            if (hit)
+            {
+                print("hit");
+                hit.transform.gameObject.SendMessageUpwards("OnTrigger", this.transform, SendMessageOptions.DontRequireReceiver);
+                SendMessageUpwards("OnTrigger", hit.transform, SendMessageOptions.DontRequireReceiver);
+                break;
+            }
+        }
+    }
+
+    void UpdateRaycastOrigins()
+    {
+        Bounds bounds = collider.bounds;
+        bounds.Expand(skinWidth * -2);
+
+        Vector3 boundsMax = transform.TransformPoint(collider.offset + collider.size / 2);
+        Vector3 boundsMin = transform.TransformPoint(collider.offset - collider.size / 2);
+
+        raycastOrigin.bottomLeft = new Vector2(boundsMin.x, boundsMin.y);
+        raycastOrigin.BottomRight = new Vector2(boundsMax.x, boundsMin.y);
+        raycastOrigin.topLeft = new Vector2(boundsMin.x, boundsMax.y);
+        raycastOrigin.topRight = new Vector2(boundsMax.x, boundsMax.y);
+    }
+
+    void CalculateRaySpacing()
+    {
+        Bounds bounds = collider.bounds;
+        bounds.Expand(skinWidth * -2);
+
+        horizontalRayCount = Mathf.Clamp(horizontalRayCount, 2, int.MaxValue);
+        VerticalRayCount = Mathf.Clamp(VerticalRayCount, 2, int.MaxValue);
+
+        horizontalRaySpacing = bounds.size.y / (horizontalRayCount - 1);
+        VecticalRaySpacing = bounds.size.x / (VerticalRayCount - 1);
+    }
+
+    struct RaycastOrigins
+    {
+        public Vector2 topLeft, topRight;
+        public Vector2 bottomLeft, BottomRight;
+    }
+
+    public struct CollisionInfo
+    {
+        public bool above, below, left, right;
+
+        public void Reset()
+        {
+            above = below = false;
+            left = right = false;
+        }
+    }
+
+}
---
+++ Assets/Player/Player.cs
@@ -0,0 +1,96 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof(Controller2D))]
+public class Player : MonoBehaviour {
+    Controller2D controller;
+
+    [HideInInspector] public bool canJump;
+    public float maxJumpHeight = 4;
+    public float minJumpHeight = 2;
+    float gravity = -20;
+    public bool gravityOn = true;
+    public float timeToJumpApex = 0.4f;
+    float maxJump = 8;
+    float minJump = 4;
+
+    public bool canMove = true;
+    public float moveSpeed = 6;
+    public float holdingSpeed = 3;
+    public float shootingSpeed = 6;
+    float velocityXSmoothing;
+    Vector3 velocity;
+
+    public float accelerationTimeAirborn = 0.2f;
+    public float acclerationTimeGrounded = 0.1f;
+
+    void Start() {
+        controller = GetComponent<Controller2D>();
+        gravity = -(2 * maxJumpHeight) / Mathf.Pow(timeToJumpApex, 2);
+        maxJump = Mathf.Abs(gravity) * timeToJumpApex;
+    }
+
+    void Update() {
+        checkIfGrounded();
+        Vector2 input = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        
+        if (canMove)
+        {
+            jumpAndHover();
+            movement(input);
+        }
+
+
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+    }
+
+    void checkIfGrounded() {
+        if (controller.collisions.below) {
+            velocity.y = Mathf.Max(0, velocity.y);
+        }
+
+        canJump = controller.collisions.below;
+
+    }
+
+    public void burstForce(Vector3 velocity, bool additive) {
+        if (additive)
+        {
+            this.velocity += velocity;
+        } else {
+            this.velocity = velocity;
+        }
+    }
+
+    void movement(Vector2 input) {
+
+        float targetSpeed =  moveSpeed;
+        targetSpeed = Input.GetButton("Fire2") ? shootingSpeed : targetSpeed;
+
+        float targetVelocityX = input.x * targetSpeed;
+        velocity.x = Mathf.SmoothDamp(velocity.x, targetVelocityX, ref velocityXSmoothing, (controller.collisions.below) ? acclerationTimeGrounded : accelerationTimeAirborn);
+
+    }
+
+    void jumpAndHover()
+    {
+        if (Input.GetButtonDown("Jump") && canJump)
+        {
+            velocity.y = maxJump;
+            canJump = false;
+        }
+
+        if (Input.GetButtonUp("Jump"))
+        {
+            if (velocity.y > minJump)
+            {
+                velocity.y = minJump;
+            }
+        }
+    }
+
+    public bool grounded() {
+        return canJump;
+    }
+}
