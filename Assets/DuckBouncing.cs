61a9a5ab576262b574fe52ca08d30f6e75f328e9
RetroSpecter <InsertNameHere4897@gmail.com>
2020-10-04 13:09:08 -0700

    new art assets

---
+++ Assets/DuckBouncing.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DuckBouncing : MonoBehaviour
+{
+
+    Vector2 initalPos;
+    public float bounceRate = 4;
+    public float bounceHeight = 0.1f;
+
+    float random;
+
+    private void Start()
+    {
+        initalPos = transform.position;
+        random = Random.RandomRange(0.0f, 1.0f);
+    }
+
+    void Update()
+    {
+        float a = Mathf.Abs(Mathf.Sin(bounceRate * Time.time + random));
+        print(a);
+        transform.position = (Vector3)initalPos + transform.up * bounceHeight * a;
+    }
+}
---
+++ Assets/TitleAndOpening.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class TitleAndOpening : MonoBehaviour
+{
+    public GameObject vCam;
+    public GameObject vCamTargetPosition;
+    private bool inGame;
+    public GameObject crashingRocket;
+    public GameObject crashedRocket;
+    // Start is called before the first frame update
+    void Start()
+    {
+        crashedRocket.SetActive(false);
+        Player.Instance.gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Z) && !inGame) {
+            PlayOpeningSequence();
+        }
+    }
+
+    void PlayOpeningSequence() {
+        Sequence s = DOTween.Sequence();
+        s.Append(crashingRocket.transform.DOMoveY(vCam.transform.position.y,1)).SetEase(Ease.Linear);
+        s.Append(vCam.transform.DOMoveY(vCamTargetPosition.transform.position.y, 1.5f)).SetEase(Ease.Linear);
+        s.Join(crashingRocket.transform.DOMoveY(vCamTargetPosition.transform.position.y - 3, 1.5f)).SetEase(Ease.Linear);
+        s.AppendCallback(() => {
+            crashingRocket.SetActive(false);
+            crashedRocket.SetActive(true);
+        });
+        s.Join(vCam.transform.DOShakePosition(1, 1));
+        s.AppendCallback(() => {
+            vCam.SetActive(false);
+        });
+
+        s.AppendInterval(2);
+
+        s.AppendCallback(() => {
+            Player.Instance.gameObject.SetActive(true);
+        });
+    }
+}
