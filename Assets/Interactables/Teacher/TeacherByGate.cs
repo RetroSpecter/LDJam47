d7f9e0eb865d7f5ba6ac4ad07d4ec07ec5f743f6
RetroSpecter <InsertNameHere4897@gmail.com>
2020-10-04 14:14:51 -0700

    added school animation

--- Assets/Interactables/Teacher/TeacherByGate.cs
+++ Assets/Interactables/Teacher/TeacherByGate.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class TeacherByGate : Interactable {
 
@@ -10,11 +11,14 @@ public class TeacherByGate : Interactable {
     public GameObject breadGuyByGate;
     public GameObject trashman;
     public GameObject graduatedBreadGuy;
+    [Space()]
+    public GameObject gate;
+    public GameObject student;
 
     // Based on the state of the game & the player's inventory, checks if the
     //  player can interact with this object. Returns if it can or not.
     protected override bool CanInteract() {
-        return GameController.Instance.CheckForEvent("BreadGuyFed") && !gateUnlocked;
+        return /*GameController.Instance.CheckForEvent("BreadGuyFed") &&*/ !gateUnlocked;
     }
 
     // If the player is concentrating on this interactable, then pressing space will
@@ -26,6 +30,21 @@ public class TeacherByGate : Interactable {
             TakeItemFromPlayer();
             gateUnlocked = true;
             StartCoroutine(this.CompleteQuest());
+
+
+            Sequence s = DOTween.Sequence();
+            s.AppendCallback(() => {
+                Player.Instance.TogglePlayerMovement(false);
+            });
+            s.Append(gate.transform.DOMove(gate.transform.position + gate.transform.right * 5, 2));
+            s.Append((transform.DOMove(transform.position + transform.right * 2, 1)));
+            s.Join(student.transform.DOMove(transform.position, 1));
+            s.Append((transform.DOMove(transform.position + transform.right * 2, 1)));
+            s.AppendCallback(() => {
+                student.SetActive(false);
+                gameObject.SetActive(false);
+                Player.Instance.TogglePlayerMovement(true);
+            });
         }
     }
 
